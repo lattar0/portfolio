07b0f1380df9d800404dbeb556913f0d947d04a9
lattar0 <lattaro@tuta.io>
2023-05-17 13:18:50 -0300

    fixing align and size of carets

--- src/components/base/ObjectContainer.tsx
+++ src/components/base/ObjectContainer.tsx
@@ -1,5 +1,5 @@
-import { CaretDown, CaretRight, DotsThreeOutline } from '@phosphor-icons/react'
-import { useState } from 'react'
+import { CaretDown, CaretRight } from '@phosphor-icons/react'
+import { useEffect, useState } from 'react'
 
 interface ObjectContainerProps {
   children: React.ReactNode
@@ -8,30 +8,40 @@ interface ObjectContainerProps {
 
 export function ObjectContainer({ children, isFlexColumn = true }: ObjectContainerProps) {
   const [isExpanded, setIsExpanded] = useState(false)
+  const [caretSize, setCaretSize] = useState(20)
+
+
+  useEffect(() => {
+    if (window !== undefined) {
+      if (window.innerWidth < 768) {
+        setCaretSize(16)
+      }
+    }
+  }, [])
 
   return (
-    <div className='flex group'>
-      <div className='mt-1.5 invisible group-hover:visible'>
-      {
-        isExpanded ? (
-          <CaretRight
-            size={20}
-            weight='fill'
-            className='cursor-pointer text-emerald-600'
-            onClick={() => setIsExpanded(false)}
-          />
-        ) : (
-          <CaretDown
-            size={20}
-            weight='fill'
-            className='cursor-pointer text-emerald-600'
-            onClick={() => setIsExpanded(true)}
-          />
-        )
-      }
+    <div className='flex group gap-2'>
+      <div className='md:mt-1.5 mt-1 invisible group-hover:visible'>
+        {
+          isExpanded ? (
+            <CaretRight
+              size={caretSize}
+              weight='fill'
+              className='cursor-pointer text-emerald-600'
+              onClick={() => setIsExpanded(false)}
+            />
+          ) : (
+            <CaretDown
+              size={caretSize}
+              weight='fill'
+              className='cursor-pointer text-emerald-600'
+              onClick={() => setIsExpanded(true)}
+            />
+          )
+        }
       </div>
-      
-      <div className={`${isFlexColumn ? 'flex-col' : ''} ${isExpanded ? 'md:max-h-7 max-h-6' : ''} flex gap-2 md:ml-4 overflow-hidden`}>
+
+      <div className={`${isFlexColumn ? 'flex-col' : ''} ${isExpanded ? 'md:max-h-7 max-h-6' : ''} flex gap-2 overflow-hidden`}>
         {children}
       </div>
     </div>
